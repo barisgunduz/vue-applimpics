701d0434dd9363d5007a54898e83fcb8bc29f501
Barış Gündüz <baris.gunduz@devstup.org>
2021-11-26 18:42:17 +0300

    move methods to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,45 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		tasks: [
+			{
+				id: 1,
+				title: "Wake Up",
+				done: false,
+			},
+			{
+				id: 2,
+				title: "Eat bananas",
+				done: false,
+			},
+			{
+				id: 3,
+				title: "do sth",
+				done: false,
+			},
+		],
+	},
+	mutations: {
+		addTask(state, newTaskTitle) {
+			let newTask = {
+				id: Date.now(),
+				title: newTaskTitle,
+				done: false,
+			};
+			state.tasks.push(newTask);
+		},
+		doneTask(state, id) {
+			let task = state.tasks.filter((task) => task.id === id)[0];
+			task.done = !task.done;
+		},
+		deleteTask(state, id) {
+			state.tasks = state.tasks.filter((task) => task.id !== id);
+		},
+	},
+	actions: {},
+	getters: {},
+});
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,23 +1,56 @@
 <template>
 	<div class="home">
-		<v-list class="pt-0" flat>
-			<div v-for="task in tasks" :key="task.id">
-				<v-list-item>
-					<template v-slot:default="{ active }">
+		<v-text-field
+			v-model="newTaskTitle"
+			@click:append="addTask"
+			@keyup.enter="addTask"
+			class="pa-3"
+			outlined
+			label="Add Task"
+			append-icon="mdi-plus"
+			hide-details
+			clearable
+		></v-text-field>
+		<v-list v-if="$store.state.tasks.length" class="pt-0" flat>
+			<div v-for="task in $store.state.tasks" :key="task.id">
+				<v-list-item
+					@click="$store.commit('doneTask', task.id)"
+					:class="{ 'blue lighten-5': task.done }"
+				>
+					<template v-slot:default>
 						<v-list-item-action>
-							<v-checkbox :input-value="active"></v-checkbox>
+							<v-checkbox :input-value="task.done"></v-checkbox>
 						</v-list-item-action>
 
 						<v-list-item-content>
-							<v-list-item-title>{{
-								task.title
-							}}</v-list-item-title>
+							<v-list-item-title
+								:class="{
+									'text-decoration-line-through': task.done,
+								}"
+								>{{ task.title }}</v-list-item-title
+							>
 						</v-list-item-content>
+						<v-list-item-action>
+							<v-btn
+								@click.stop="
+									$store.commit('deleteTask', task.id)
+								"
+								icon
+							>
+								<v-icon color="primary lighten-1"
+									>mdi-delete</v-icon
+								>
+							</v-btn>
+						</v-list-item-action>
 					</template>
 				</v-list-item>
 				<v-divider></v-divider>
 			</div>
 		</v-list>
+		<div v-else class="no-tasks">
+			<v-icon size="100" color="primary">mdi-check</v-icon>
+			<div class="text-h5 primary--text">No tasks</div>
+		</div>
 	</div>
 </template>
 
@@ -25,20 +58,22 @@
 export default {
 	name: "Todo",
 	data: () => ({
-		tasks: [
-			{
-				id: 1,
-				title: "Wake Up",
-			},
-			{
-				id: 2,
-				title: "Eat bananas",
-			},
-			{
-				id: 3,
-				title: "do sth",
-			},
-		],
+		newTaskTitle: "",
 	}),
+	methods: {
+		addTask() {
+			this.$store.commit("addTask", this.newTaskTitle);
+			this.newTaskTitle = "";
+		},
+	},
 };
 </script>
+
+<style lang="sass">
+.no-tasks
+	position: absolute
+	left: 50%
+	top: 50%
+	transform: translate(-50%, -50%)
+	opacity: 0.5
+</style>
